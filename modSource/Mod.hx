5de33ac611ca171d31ee533d2ff0a5157461af22
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-04-18 23:20:16 -0400

    Update Project.xml
    Updated Project.xml

--- modSource/Mod.hx
+++ modSource/Mod.hx
@@ -1,7 +1,10 @@
 import CrewState;
 
+import lime.utils.Assets;
+
 class Mod {
     static public function onInit() {
         Register.attachLuaToState(MainMenuState, new ModLua(Paths.lua("jtc_menu")));
+        //trace(Assets.getText("config/mod.json"));
     }
 }
