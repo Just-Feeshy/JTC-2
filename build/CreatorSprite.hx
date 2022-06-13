fac9421803f00905ac368944eb107d9e88ae9b9c
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-06-13 09:51:35 -0400

    Deleted useless class

--- build/CreatorSprite.hx
+++
@@ -1,9 +0,0 @@
-package;
-
-import flixel.FlxSprite;
-
-/**
-* This sprite class was made to make creator/editior states easier to configure
-* which would also allow the user to easily cutomize said sprite for there mod.
-*/
-class EditiorSprite
