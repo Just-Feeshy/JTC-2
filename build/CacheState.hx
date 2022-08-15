4a63a08c917c7a4b0976fc3b23d8828613b96d01
Diego <fonseca.diego@outlook.com>
2022-08-15 08:17:19 -0400

    Remove that

--- build/CacheState.hx
+++ build/CacheState.hx
@@ -115,15 +115,6 @@ class CacheState extends MusicBeatState {
     static public function loadAndSwitchState(target:FlxState, ?stopMusic:Bool = true, ?exception:Bool = false):Void {
         Paths.setCurrentLevel("week" + PlayState.storyWeek);
 
-        #if NO_PRELOAD_ALL
-		var loaded = isSoundLoaded(getSongPath())
-			&& (!PlayState.SONG.needsVoices || isSoundLoaded(getVocalPath()))
-			&& isLibraryLoaded("shared");
-		
-		if (!loaded)
-			return new LoadingState(target, stopMusic);
-		#end
-
 		if (stopMusic && FlxG.sound.music != null) {
 			FlxG.sound.music.stop();
         }
