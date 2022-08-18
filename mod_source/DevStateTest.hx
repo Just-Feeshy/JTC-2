40c1fbde82a81f60336454e10d105367708370f3
Diego <fonseca.diego@outlook.com>
2022-08-18 19:04:28 -0400

    HAHA!

--- mod_source/DevStateTest.hx
+++ mod_source/DevStateTest.hx
@@ -1,11 +1,24 @@
 package;
 
+import flixel.FlxG;
+import openfl.events.Event;
+
 class DevStateTest extends HelperStates {
     public function new() {
         super();
+
+        FlxG.stage.addEventListener(Event.ENTER_FRAME, enterFrame);
     }
 
     override function create():Void {
         super.create();
+    }
+
+    function enterFrame(_):Void {
+        trace("do a silly");
+
+        if(FlxG.keys.justPressed.P) {
+            throw "haha you suck!";
+        }
     }
 }
