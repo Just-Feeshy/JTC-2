1b4a06d7d050d4a11e0eca292099edc4962fb958
Diego <fonseca.diego@outlook.com>
2022-12-10 17:03:51 -0500

    hmmmm

--- mod_assets/scripts/stage/funkroad.lua
+++ mod_assets/scripts/stage/funkroad.lua
@@ -3,8 +3,6 @@
 
 --variables
 local frost_modchart = nil
-local skaterboi = nil
-local skaterboi_extra = nil
 
 local beatSection = 0
 
@@ -29,38 +27,18 @@ function generatedStage()
 
     --setupPunchHealth(3)
 
-    skaterboi = require("mod_assets/scripts/modules/character")
-    skaterboi.createNFA("skater-boi-player", "skater-boi-player", 650, 100, false)
-    skaterboi.initSing(jtcStrumAnims)
-
-    --skaterboi_extra = require("mod_assets/scripts/modules/character")
-    --skaterboi_extra.create("skater-boi-extra", "skater-boi-extra", 650, 100, true)
-    --skaterboi_extra.initSing(jtcStrumAnims)
-
     frost_modchart = require("mod_assets/scripts/modcharts/frost_modchart")
     frost_modchart.initStrumsAndNotes()
-end
-
-function updateCharacter()
-    if skaterboi ~= nil and daddyIsHere then
-        skaterboi.dance()
-    end
 end
 
 function onStepHit()
     if curStep == 630 and not daddyIsHere then
         callEvent("character change", "dad-car", "dad")
-        addSpriteToStage("skater-boi-player")
 
         removeSpriteFromState("frostbiteCAR")
         destroySprite("frostbiteCAR")
 
         daddyIsHere = true
-    end
-
-    if curStep == 631 then
-        removeSpriteToStage("boyfriend")
-        insertSpriteToStage(getSpriteIndexFromStage("skater-boi-player") + 1, "boyfriend")
     end
 
     if curStep == 147 and beatSection == 0 then
@@ -115,8 +93,6 @@ function onUpdate(elapsed)
             frost_modchart.sectionTwo_REGULAR()
         end
     end
-
-    updateCharacter()
 end
 
 --functions
