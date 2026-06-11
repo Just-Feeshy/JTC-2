535828c150e34f728dbc991c25043bf18b572df9
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2026-06-10 21:28:40 -0400

    new thing

--- mod_source/NotesJTC.hx
+++ mod_source/NotesJTC.hx
@@ -142,7 +142,7 @@ class DeathNote extends CustomNoteTemplate {
  */
 class CheeseFifthNote extends CustomNoteTemplate {
 	public static var fifthKeySize:Float = 1.0;
-	public static var fifthKeyOffsetX:Float = 0;
+	public static var fifthKeyOffsetX:Float = -18;
 
     override function getCustomAssetPath():String {
         return "fifth/JTC_SPECIAL_NOTE_assets";
